d76101c1e6865fdbfe5b07863a7db74c20afe993
Li Bo <libo@espressif.com>
2021-06-29 19:12:16 +0800

    device(msc):clean code

--- examples/usb_device/usb_disk/main/main.c
+++ examples/usb_device/usb_disk/main/main.c
@@ -8,10 +8,9 @@
 #include "esp_system.h"
 #include "esp_spiffs.h"
 #include "nvs_flash.h"
-#include "diskio_wl.h"
 #include "esp_vfs.h"
 #include "esp_vfs_fat.h"
-#include "driver/gpio.h"
+
 #include "driver/sdmmc_defs.h"
 #include "driver/sdmmc_types.h"
 #include "driver/sdspi_host.h"
@@ -21,22 +20,29 @@
 #include "bsp_esp32_s3_usb_otg_ev.h"
 #include "tusb_msc.h"
 
-static sdmmc_card_t *mount_card = NULL;
-static const char *TAG = "usb_demo";
+static const char *TAG = "usb_msc_demo";
 
 #ifdef CONFIG_USE_EXTERNAL_SDCARD
-#define BOARD_SDCARD_MOSI_PIN CONFIG_SDCARD_MOSI_PIN
-#define BOARD_SDCARD_MISO_PIN CONFIG_SDCARD_MISO_PIN
-#define BOARD_SDCARD_SCLK_PIN CONFIG_SDCARD_SCLK_PIN
-#ifdef SOC_SDMMC_HOST_SUPPORTED
+#ifdef CONFIG_SDCARD_INTFC_SPI
+#define BOARD_SDCARD_SPI_MOSI_PIN CONFIG_SDCARD_MOSI_PIN
+#define BOARD_SDCARD_SPI_MISO_PIN CONFIG_SDCARD_MISO_PIN
+#define BOARD_SDCARD_SPI_SCLK_PIN CONFIG_SDCARD_SCLK_PIN
+#define BOARD_SDCARD_SPI_CS_PIN 26
+#elif defined(CONFIG_SDCARD_INTFC_SDIO) && defined(SOC_SDMMC_HOST_SUPPORTED)
+#define BOARD_SDCARD_SDIO_CLK_PIN 33
+#define BOARD_SDCARD_SDIO_CMD_PIN 35
+#define BOARD_SDCARD_SDIO_DO_PIN 34
+#define BOARD_SDCARD_SDIO_DATA_WIDTH 1
 #include "driver/sdmmc_host.h"
+#else
+#error "Not supported interface"
 #endif
 #endif
-
-#define CONFIG_USE_SDSPI
 
 // Mount path for the partition
+static sdmmc_card_t *mount_card = NULL;
 const char *base_path = "/disk";
+
 /* Function to initialize SPIFFS */
 static esp_err_t init_fat(sdmmc_card_t **card_handle)
 {
@@ -45,7 +51,7 @@ static esp_err_t init_fat(sdmmc_card_t **card_handle)
     // To mount device we need name of device partition, define base_path
     // and allow format partition in case if it is new one and was not formated before
 #ifdef CONFIG_USE_INTERNAL_FLASH
-// Handle of the wear levelling library instance
+    // Handle of the wear levelling library instance
     wl_handle_t wl_handle_1 = WL_INVALID_HANDLE;
     ESP_LOGI(TAG, "using internal flash");
     const esp_vfs_fat_mount_config_t mount_config = {
@@ -69,33 +75,37 @@ static esp_err_t init_fat(sdmmc_card_t **card_handle)
         .allocation_unit_size = CONFIG_DISK_BLOCK_SIZE
     };
 
-#ifdef CONFIG_USE_SDSPI
-    ESP_LOGI(TAG, "Using SPI peripheral");
+#ifdef CONFIG_SDCARD_INTFC_SPI
+    ESP_LOGI(TAG, "Using SPI Interface");
     sdmmc_host_t host = SDSPI_HOST_DEFAULT();
     host.slot = SPI3_HOST;
     sdspi_device_config_t slot_config = SDSPI_DEVICE_CONFIG_DEFAULT();
-    slot_config.gpio_cs = 26;
+    slot_config.gpio_cs = BOARD_SDCARD_SPI_CS_PIN;
     slot_config.host_id = host.slot;
 
     ret = esp_vfs_fat_sdspi_mount(base_path, &host, &slot_config, &mount_config, &card);
-#else
-    ESP_LOGI(TAG, "Using SDMMC peripheral");
+
+#elif defined(CONFIG_SDCARD_INTFC_SDIO) && defined(SOC_SDMMC_HOST_SUPPORTED)
+    ESP_LOGI(TAG, "Using SDIO Interface");
     sdmmc_host_t host = SDMMC_HOST_DEFAULT();
 
     // This initializes the slot without card detect (CD) and write protect (WP) signals.
     // Modify slot_config.gpio_cd and slot_config.gpio_wp if your board has these signals.
     sdmmc_slot_config_t slot_config = SDMMC_SLOT_CONFIG_DEFAULT();
-    slot_config.clk = 15;
-    slot_config.cmd = 7;
-    slot_config.d0 = 4;
+    slot_config.clk = BOARD_SDCARD_SDIO_CLK_PIN;
+    slot_config.cmd = BOARD_SDCARD_SDIO_CMD_PIN;
+    slot_config.d0 = BOARD_SDCARD_SDIO_DO_PIN;
     // To use 1-line SD mode, change this to 1:
-    slot_config.width = 1;
+    slot_config.width = BOARD_SDCARD_SDIO_DATA_WIDTH;
     // Enable internal pullups on enabled pins. The internal pullups
     // are insufficient however, please make sure 10k external pullups are
     // connected on the bus. This is for debug / example purpose only.
     //slot_config.flags |= SDMMC_SLOT_FLAG_INTERNAL_PULLUP;
 
     ret = esp_vfs_fat_sdmmc_mount(base_path, &host, &slot_config, &mount_config, &card);
+#else
+#error "Not supported interface"
+    return ESP_ERR_NOT_SUPPORTED;
 #endif
 
     if (ret != ESP_OK) {
@@ -122,37 +132,12 @@ static esp_err_t init_fat(sdmmc_card_t **card_handle)
     return ESP_OK;
 }
 
-static void button_ok_single_click_cb(void *arg)
-{
-    ESP_LOGI(TAG, "BTN OK: BUTTON_SINGLE_CLICK");
-}
-
-static void button_up_single_click_cb(void *arg)
-{
-    ESP_LOGI(TAG, "BTN UP: BUTTON_SINGLE_CLICK");
-}
-
-static void button_dw_single_click_cb(void *arg)
-{
-    ESP_LOGI(TAG, "BTN DW: BUTTON_SINGLE_CLICK");
-}
-
-static void button_menu_single_click_cb(void *arg)
-{
-    ESP_LOGI(TAG, "BTN MENU: BUTTON_SINGLE_CLICK");
-}
-
 void app_main(void)
 {
     iot_board_init();
     iot_board_usb_set_mode(USB_DEVICE_MODE);
     iot_board_usb_device_set_power(true, true);
     /* Initialize file storage */
-
-    iot_board_button_register_cb(iot_board_get_handle(BOARD_BTN_OK_ID), BUTTON_SINGLE_CLICK, button_ok_single_click_cb);
-    iot_board_button_register_cb(iot_board_get_handle(BOARD_BTN_UP_ID), BUTTON_SINGLE_CLICK, button_dw_single_click_cb);
-    iot_board_button_register_cb(iot_board_get_handle(BOARD_BTN_DW_ID), BUTTON_SINGLE_CLICK, button_up_single_click_cb);
-    iot_board_button_register_cb(iot_board_get_handle(BOARD_BTN_MN_ID), BUTTON_SINGLE_CLICK, button_menu_single_click_cb);
 
     ESP_ERROR_CHECK(init_fat(&mount_card));
     vTaskDelay(100 / portTICK_PERIOD_MS);
@@ -170,14 +155,12 @@ void app_main(void)
     ESP_ERROR_CHECK(tinyusb_driver_install(&tusb_cfg));
     ESP_ERROR_CHECK(tusb_msc_init(&msc_cfg));
     ESP_LOGI(TAG, "USB initialization DONE");
+
     bool led_state = false;
-    while (1) { //TODO: monitor system if you want
-        float v1 = iot_board_get_host_voltage();
-        float v2 = iot_board_get_battery_voltage();
-        ESP_LOGW(TAG, "Host voltage=%.2f, Battery voltage=%.2f", v1, v2);
+    while (1) {
         iot_board_led_all_set_state(led_state);
         led_state = !led_state;
-        vTaskDelay(2000 / portTICK_RATE_MS);
+        vTaskDelay(1000 / portTICK_RATE_MS);
     }
 
 }
